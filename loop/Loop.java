93ae76ae768b1a2ddaba187e220b8a995131120c
karthikut <101128009+karthikut@users.noreply.github.com>
2022-03-15 07:20:33 +0530

    forloop

---
+++ loop/Loop.java
@@ -0,0 +1,13 @@
+class Loop{
+	public static void main(String[] args){
+		
+		String[] subject={"kannada", "hindi", "english", "maths", "science"};
+		
+		for (int i=subject.length-1;0<=i;i--){
+			
+			System.out.println(subject[i]);
+			
+		}
+		
+	}
+}
---
+++ loop/Loop1.java
@@ -0,0 +1,13 @@
+class Loop1{
+	public static void main(String[] args){
+		
+		String[] subject={"kannada", "hindi", "english", "maths", "science"};
+		
+		for (int i=0;i<5;i++){
+			
+			System.out.println(subject[i]);
+			
+		}
+		
+	}
+}
