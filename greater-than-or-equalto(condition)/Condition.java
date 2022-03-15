7969a2c94b94bde43a993b4d42bd8f65b5408529
karthikut <101128009+karthikut@users.noreply.github.com>
2022-03-15 07:06:38 +0530

    conditions

---
+++ greater-than-or-equalto(condition)/Condition.java
@@ -0,0 +1,13 @@
+class Condition{
+public static void main(String[] args){
+int abhi=3000;
+
+
+if (abhi<=2000){
+System.out.println("abhi is a good boy");	
+}
+else{
+	System.out.println("abhi is so voilent");
+}
+}
+}
---
+++ greater-than-or-equalto(condition)/Condition1.java
@@ -0,0 +1,12 @@
+class Condition1{
+public static void main(String[] args){
+	int raju=19;
+	
+	if(raju==19){
+		System.out.println("raju born on 19th of february");
+	}
+if(raju>=19){	
+	System.out.println("happywala bdy raju");	
+	}
+}
+}
